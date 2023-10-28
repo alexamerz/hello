9839aea0ee838742b0c32757e2a4b7223b642d25
alexamerz <144001880+alexamerz@users.noreply.github.com>
2023-10-28 11:02:41 +0000

    Sat, Oct 28, 2023, 6:02 PM +07:00

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
 @app.route("/greet", methods=["POST"])
 def greet():
-    first_name = request.form.get("first_name")
+    first_name = request.form.get("first_name", "world")
     return render_template("greet_html", first_name = first_name)
